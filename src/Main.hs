abb094594b9bf6733d31064ef7bd37b964f319b1
Shunji Nishimura <s-nishimura@oita-ct.ac.jp>
2017-09-28 10:29:58 +0900

    mod: reply first

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-} {- -*- Coding: utf-8 -*- -}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- > heroku buildpacks:set https://github.com/mfine/heroku-buildpack-stack.git
 -- > heroku config:set CHANNEL_SECRET={Channel Secret}
@@ -18,15 +18,11 @@ import Network.Wai
 import Network.Wai.Handler.Warp (run)
 
 import Line.Messaging.API
-import Line.Messaging.Webhook ( Event(..), EventMessage(..), ReplyToken(..)
-                              , ReplyableEvent(..), webhookApp
-                              , defaultOnFailure, getMessage, getReplyToken)
-import Line.Messaging.Webhook.Types
-import Line.Messaging.Types (Text(..), getText)
+import Line.Messaging.Webhook
 import Line.Messaging.Common.Types (ID)
 
 type UserID = ID
-type MV = MVar (Maybe (UserID, ReplyToken))
+type MV = MVar (UserID, T.Text)
 
 getChannelSecret :: IO ChannelSecret
 getChannelSecret = T.pack <$> getEnv "CHANNEL_SECRET"
@@ -37,7 +33,7 @@ getChannelToken = T.pack <$> getEnv "CHANNEL_TOKEN"
 main :: IO ()
 main = do
   port <- maybe 80 read <$> lookupEnv "PORT" :: IO Int
-  mv <- newMVar Nothing
+  mv <- newMVar ("", "システムより：\n再起動しました")
   run port $ lineBot mv
 
 lineBot :: MV -> Application
@@ -54,22 +50,20 @@ handleEvent _ _ = return ()
 
 handleMessageEvent :: MV -> ReplyableEvent EventMessage -> IO ()
 handleMessageEvent mv event = do
-  let replyToken = getReplyToken event
+  let
+    userID = getReplyToken event
+    replyToken = getID $ getSource event
   case getMessage event of
-    TextEM mid (Text text) -> do
-      -- echo replyToken text -- [debug]
-      let userID = getID $ getSource event
-      mbU'R <- takeMVar mv
-      when (isJust mbU'R) $ do
-        let (userID', replyToken') = fromJust mbU'R
-        when (userID' /= userID) $ echo replyToken' text
-      putMVar mv $ Just (userID, replyToken)
-    _ -> echo replyToken "システムより：すみません、それには対応していません"
+    TextEM mid (Text word) -> do
+      (userID', word') <- takeMVar mv
+      when (userID' /= userID) $ echo replyToken word'
+      putMVar mv (userID, word)
+    _ -> echo replyToken "システムより：\nすみません、それには対応していません"
 
 api :: APIIO a -> IO (Either APIError a)
 api = runAPI getChannelToken
 
 echo :: ReplyToken -> T.Text -> IO ()
-echo replyToken content = do
-  api $ reply replyToken [ Message . Text $ content ]
+echo replyToken word = do
+  api $ reply replyToken [ Message $ Text word ]
   return ()
